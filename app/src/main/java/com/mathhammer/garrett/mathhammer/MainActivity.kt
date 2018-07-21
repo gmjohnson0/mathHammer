d82ba598dde8e214952ecdddb1308594e416e140
gmjohnson0 <gmjohnson0@gmail.com>
2018-07-21 00:24:21 -0500

    Initial commit

---
+++ app/src/main/java/com/mathhammer/garrett/mathhammer/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mathhammer.garrett.mathhammer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun launchManualActivity(view: View) {
+
+    }
+}
---
+++ app/src/main/java/com/mathhammer/garrett/mathhammer/ManualActivity.kt
@@ -0,0 +1,12 @@
+package com.mathhammer.garrett.mathhammer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ManualActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual)
+    }
+}
